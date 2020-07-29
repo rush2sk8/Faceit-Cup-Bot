eec711b03d4181d2c8b5d6775c8e6856e9e83575
rushadantia <rushadantia@gmail.com>
2020-07-29 19:33:56 -0400

    spam protection and QOL stuff

--- bot.js
+++ bot.js
@@ -8,8 +8,12 @@ let CUP_CHANNEL2 = process.env.CUP_CHANNEL2
 let CUP_ROLE = process.env.CUP_ROLE
 let REACTION_EMOJI = process.env.REACTION_EMOJI
 
+var messages = []
+var time = Date.now()
+var firstRun = true
+
 bot.on('ready', function(evt) {
-    bot.user.setActivity("Created by rush2sk8", { type: "STREAMING", url: "https://www.twitch.tv/rov1" })
+    bot.user.setActivity("Created by rush2sk8", { type: "STREAMING", url: "https://www.twitch.tv/rush2sk8" })
 })
 
 bot.on('message', (message) => {
@@ -20,36 +24,47 @@ bot.on('message', (message) => {
 
     //view all of the messages and look for a twitch clip link
     if (channelName == CUP_CHANNEL || channelName == CUP_CHANNEL2) {
-        if (content == "!cup") {
-            message.channel.send("<@&" + CUP_ROLE + "> Please react to this if you want to play in the cup.").then((m) => { m.react(REACTION_EMOJI) })
+        if (content.startsWith("!cup")) {
+            if (Date.now() - time >= 300000 || firstRun) {
+                message.channel.send("<@&" + CUP_ROLE + "> Please react to this if you want to play in the cup.").then((m) => {
+                    m.react(REACTION_EMOJI)
+                    messages.push(m.id)
+                    time = Date.now()
+                    firstRun = false
+                })
+            } else {
+                message.channel.send("**Please wait " + ((300000 - (Date.now() - time)) / 60000.0).toFixed(2) + " mins before starting a new cup**")
+            }
         }
     }
 })
 
 bot.on('messageReactionAdd', (reaction, user) => {
+    if (messages.includes(reaction.message.id)) {
+        if (reaction.emoji.name != REACTION_EMOJI) reaction.remove(user)
+        if (reaction.emoji.name == REACTION_EMOJI && reaction.count > 6) {
+            reaction.remove(user)
+            return
+        }
 
-    if (reaction.emoji.name != REACTION_EMOJI) reaction.remove(user)
-    if (reaction.emoji.name == REACTION_EMOJI && reaction.count > 6) {
-        reaction.remove(user)
-        return
-    }
+        if (reaction.count == 6) {
+            let message = reaction.message
+            const users = reaction.users.map(u => u.toString()).slice(1).toString()
+            
+            let msgJump = "https://discordapp.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id
 
-    if (reaction.count == 6) {
-        let message = reaction.message
-        const users = reaction.users.map(u => u.toString()).slice(1).toString()
-
- 		console.log(message.guild.id)
-        console.log(message.channel.id)
-        console.log(message.id)
-
-        let msgJump = "https://discordapp.com/channels/"+ message.guild.id + "/" + message.channel.id + "/" + message.id
-
-        const embed = new Discord.RichEmbed()
-            .setTitle("Faceit Cup Team ")
-            .setColor("#ffbb00")
-            .setDescription("The [team](" + msgJump + ") will consist of: \n" + users)
-            .setThumbnail("https://res-3.cloudinary.com/crunchbase-production/image/upload/xtrxobrlttwixe1kfzo5")
-            .setTimestamp()
-        message.channel.send(embed)
+            const embed = new Discord.RichEmbed()
+                .setTitle("Faceit Cup Team ")
+                .setColor("#ffbb00")
+                .setDescription("The [team](" + msgJump + ") will consist of: \n" + users)
+                .setThumbnail("https://res-3.cloudinary.com/crunchbase-production/image/upload/xtrxobrlttwixe1kfzo5")
+                .setTimestamp()
+            message.channel.send(embed)
+            firstRun = true
+        }
     }
 })
+
+setInterval(() => {
+    messages.shift()
+}, 21600000)
